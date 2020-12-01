f1933791cdb8df67d11ca065cfbcc0ceee6c8669
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-01 14:53:35 +0100

    Feature/day1 (#1)
    Solution for day01 part 1 and 2

---
+++ Aoc2020.Lib/Day01/ExpenseReport.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day01
+{
+    public class ExpenseReport
+    {
+        public long Part1(int[] lines, int target = 2020)
+        {
+            var start = 0; var end = lines.Length - 1;
+            while (start < end)
+            {
+                var sum = lines[start] + lines[end];
+                if (sum == target)
+                {
+                    return lines[start] * lines[end];
+                }
+                if (sum > target)
+                {
+                    end--;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+
+            throw new Exception($"could not find target: {target}");
+        }
+        public long Part2(int[] lines, int start, int end, int target = 2020)
+        {
+            if (start >= end)
+            {
+                throw new Exception("Start is greater than end");
+            }
+            var next = start + 1;
+            var sum = lines[start] + lines[end];
+            while (next < end)
+            {
+                var total = sum + lines[next];
+                if (total == target)
+                {
+                    return lines[start] * lines[end] * lines[next];
+                }
+                if (total < target)
+                {
+                    next++;
+                }
+                else
+                {
+                    return Part2(lines, start, end - 1);
+                }
+            }
+            return Part2(lines, start + 1, end);
+        }
+    }
+}
---
+++ Aoc2020.Tests/Day01/Day01Tests.cs
@@ -0,0 +1,45 @@
+﻿using Aoc2020.Lib.Day01;
+using Aoc2020.Lib.Util;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests.Day01
+{
+    public class Day01Tests
+    {
+        [Theory]
+        [InlineData("Day01/Example1.txt", 514579)]
+        [InlineData("Day01/Input.txt", 980499)]
+        public void Part1(string filepath, int expected)
+        {
+            var sut = new ExpenseReport();
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory()).ToList();
+            lines.Sort();
+            var actual = sut.Part1(lines.ToArray());
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Day01/Example1.txt", 241861950)]
+        [InlineData("Day01/Input.txt", 200637446)]
+        public void Part2(string filepath, int expected)
+        {
+            var sut = new ExpenseReport();
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory()).ToList();
+            lines.Sort();
+            var actual = sut.Part2(lines.ToArray(), 0, lines.ToArray().Length - 1);
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class IdentityFactory : IParseFactory<int>
+    {
+        public int Create(string raw)
+        {
+            return Convert.ToInt32(raw);
+        }
+    }
+}
