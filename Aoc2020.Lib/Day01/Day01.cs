12d8c93903d3ea71c84b7d2474aadc53995a10e1
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-01 07:37:41 +0100

    Solution for day01 part 1 and 2

---
+++ Aoc2020.Lib/Day01/Day01.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace Aoc2020.Lib.Day01
+{
+    public class Day01
+    {
+        public long Part1(int[] lines)
+        {
+            var length = lines.Count();
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = i + 1; j < length; j++)
+                {
+                    if (lines[i] + lines[j] == 2020)
+                    {
+                        return lines[i] * lines[j];
+                    }
+                }
+            }
+            return 0;
+        }
+        public long Part2(int[] lines)
+        {
+            var length = lines.Count();
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = i + 1; j < length; j++)
+                {
+                    for (var k = j + 1; k < length; k++)
+                    {
+                        if (lines[i] + lines[j] + lines[k] == 2020)
+                        {
+                            return lines[i] * lines[j] * lines[k];
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Aoc2020.Tests/Day01/Day01Tests.cs
@@ -0,0 +1,42 @@
+﻿using Aoc2020.Lib.Util;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests.Day01
+{
+    public class Day01Tests
+    {
+        [Theory]
+        [InlineData("Day01/Example1.txt", 514579)]
+        [InlineData("Day01/Input.txt", 980499)]
+        public void Part1(string filepath, int expected)
+        {
+            var sut = new Aoc2020.Lib.Day01.Day01();
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory());
+            var actual = sut.Part1(lines.ToArray());
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Day01/Example1.txt", 241861950)]
+        [InlineData("Day01/Input.txt", 200637446)]
+        public void Part2(string filepath, int expected)
+        {
+            var sut = new Aoc2020.Lib.Day01.Day01();
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory());
+            var actual = sut.Part2(lines.ToArray());
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class IdentityFactory : IParseFactory<int>
+    {
+        public int Create(string raw)
+        {
+            return Convert.ToInt32(raw);
+        }
+    }
+}
