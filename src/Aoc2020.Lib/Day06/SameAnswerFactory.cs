926c1d102cbc6e28d7ca6e4c25c2a166b4f272da
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-06 19:13:26 +0100

    Feature/day06 (#6)
    Solution for day 6

---
+++ src/Aoc2020.Lib/Day06/SameAnswerFactory.cs
@@ -0,0 +1,46 @@
+﻿using Aoc2020.Lib.Util;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day06
+{
+    public class SameAnswerFactory : IParseFactory<HashSet<char>>
+    {
+        private List<string> answers;
+
+        public HashSet<char> Create(Line line)
+        {
+            if (answers == null)
+            {
+                answers = new List<string>();
+            }
+
+            if (string.IsNullOrEmpty(line.Raw))
+            {
+                var same = GroupAnswers();
+                this.answers = null;
+                return same;
+            }
+
+            answers.Add(line.Raw);
+            
+            if (line.LastLine)
+            {
+                return GroupAnswers();
+            }
+
+            return null;
+        }
+
+        private HashSet<char> GroupAnswers()
+        {
+            return this.answers
+                .Select(c => new HashSet<char>(c))
+                .Aggregate((acc, next) =>
+                   {
+                       acc.IntersectWith(new HashSet<char>(next));
+                       return acc;
+                   });
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day06/Day06Tests.cs
@@ -0,0 +1,40 @@
+﻿using Aoc2020.Lib.Day06;
+using Aoc2020.Lib.Util;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests.Day06
+{
+    public class Day06Tests
+    {
+        [Theory]
+        [InlineData("Day06/Example1.txt", 11)]
+        [InlineData("Day06/Input.txt", 7128)]
+        public void Part1(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var answers = parser.Parse(new AnswerFactory()).Where(c => c != null);
+            var actual = answers.Aggregate(0, (acc, next) =>
+            {
+                acc += next.Count;
+                return acc;
+            });
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Day06/Example1.txt", 6)]
+        [InlineData("Day06/Input.txt", 3640)]
+        public void Part2(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var answers = parser.Parse(new SameAnswerFactory()).Where(c => c != null);
+            var actual = answers.Aggregate(0, (acc, next) =>
+            {
+                acc += next.Count;
+                return acc;
+            });
+            Assert.Equal(expected, actual);
+        }
+    }
+}
