b3932aa6bb09947d7d8ad3c1d4d3a35301f2887b
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-07 16:17:53 +0100

    Feature/day07 (#7)
    Solution for day7

---
+++ src/Aoc2020.Lib/Day07/LuggageProcessor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day07
+{
+    public class LuggageProcessor
+    {
+        private IDictionary<string, Bag[]> bags;
+
+        public LuggageProcessor(IDictionary<string, Bag[]> bags)
+        {
+            this.bags = bags;
+        }
+
+        public int Process(string name)
+        {
+            var result = new HashSet<string>();
+            var queue = new Queue<string>();
+            foreach(var bag in GetContainingBags(name))
+            {
+                result.Add(bag);
+                queue.Enqueue(bag);
+            }
+
+            while (queue.Count > 0)
+            {
+                var bagName = queue.Dequeue();
+                foreach (var bag in GetContainingBags(bagName))
+                {
+                    if (HasBags(bag))
+                    {
+                        queue.Enqueue(bag);
+                        result.Add(bag);
+                    }
+                }
+
+            }
+            return result.Count;
+        }
+
+        public int Pack(string name)
+        {
+            var cost = new Dictionary<string, int>();
+            var queue = new Queue<string>();
+            var leafs = GetContainingBags("no other");
+            var visited = new HashSet<string>();
+            foreach (var leaf in leafs)
+            {
+                queue.Enqueue(leaf);
+                cost.Add(leaf, 0);
+                visited.Add(leaf);
+            }
+
+            while (queue.Count > 0)
+            {
+                var next = GetContainingBags(queue.Dequeue());
+                foreach (var n in next)
+                {
+                    var bag = this.bags[n];
+                    if (HasRequired(bag, cost) && !visited.Contains(n))
+                    {
+                        cost.Add(n, bag.Sum(b => b.Amount + b.Amount * cost[b.Name]));
+                        visited.Add(n);
+                        queue.Enqueue(n);
+                    }
+                }
+            }
+
+            return cost[name];
+        }
+
+        private bool HasRequired(Bag[] bag, Dictionary<string, int> cost)
+        {
+            return bag.All(b => cost.ContainsKey(b.Name));
+        }
+
+        private bool HasBags(string bag)
+        {
+            return string.Compare(bag, "no other", StringComparison.CurrentCultureIgnoreCase) != 0;
+        }
+
+        private IEnumerable<string> GetContainingBags(string name)
+        {
+            foreach (var bag in this.bags)
+            {
+                if (bag.Value.Any(b => b.Name == name))
+                {
+                    yield return bag.Key;
+                }
+            }
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day07/Day07Tests.cs
@@ -0,0 +1,74 @@
+﻿using Aoc2020.Lib.Day07;
+using Aoc2020.Lib.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Aoc2020.Tests.Day07
+{
+    public class Day07Tests
+    {
+        [Theory]
+        [InlineData("Day07/Example1.txt", 4)]
+        [InlineData("Day07/Input.txt", 211)]
+        public void Part1(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var bags = parser.Parse(new BagFactory());
+            var sut = new LuggageProcessor(new Dictionary<string, Bag[]>(bags));
+            var actual = sut.Process("shiny gold");
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Day07/Example1.txt", 32)]
+        [InlineData("Day07/Example2.txt", 126)]
+        [InlineData("Day07/Input.txt", 12414)]
+        public void Part2(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var bags = parser.Parse(new BagFactory());
+            var sut = new LuggageProcessor(new Dictionary<string, Bag[]>(bags));
+            var actual = sut.Pack("shiny gold");
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    internal class BagFactory : IParseFactory<KeyValuePair<string, Bag[]>>
+    {
+        public KeyValuePair<string, Bag[]> Create(Line line)
+        {
+            var segments = line.Raw.Split(new[] { "bags", "bag", "contain", ".", "," }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            var bags = new List<Bag>();
+            foreach (var seg in segments.Skip(1))
+            {
+                var matches = Regex.Matches(seg, @"(\d+)?([a-zA-Z\s]+)");
+                foreach (Match match in matches)
+                {
+                    if (int.TryParse(match.Groups[1].Value, out int amount))
+                    {
+                        bags.Add(new Bag
+                        {
+                            Amount = amount,
+                            Name = match.Groups[2].Value.Trim(),
+                        });
+                    }
+                    else
+                    {
+                        bags.Add(new Bag
+                        {
+                            Amount = 1,
+                            Name = match.Groups[2].Value.Trim(),
+                        });
+
+                    }
+                }
+            }
+
+            return KeyValuePair.Create(segments[0], bags.ToArray());
+        }
+    }
+}
