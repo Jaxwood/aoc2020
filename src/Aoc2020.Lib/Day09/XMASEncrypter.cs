3ddf7d1fd0a78247e47bc2b4319d51d8696f7a52
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-09 07:17:11 +0100

    Soution for part 1

---
+++ src/Aoc2020.Lib/Day09/XMASEncrypter.cs
@@ -0,0 +1,48 @@
+﻿using Aoc2020.Lib.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day09
+{
+    public class XMASEncrypter
+    {
+        private readonly long[] lines;
+
+        public readonly int preamble;
+
+        public XMASEncrypter(long[] lines, int preamble)
+        {
+            this.lines = lines;
+            this.preamble = preamble;
+        }
+
+        public Result<long> Cypher()
+        {
+            for (int i = 0; i < this.lines.Length - this.preamble; i++)
+            {
+                long target = this.lines[i + this.preamble];
+                if (!CanSumTo(i, target))
+                {
+                    return new Success<long>(target);
+                }
+            }
+
+            return new Failure<long>();
+        }
+
+        private bool CanSumTo(int index, long target)
+        {
+            var sum = new HashSet<long>();
+            for (int i = 0; i < this.preamble; i++)
+            {
+                if (sum.Contains(target - this.lines[index + i]))
+                {
+                    return true;
+                }
+                sum.Add(this.lines[index + i]);
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day09/Day09Tests.cs
@@ -0,0 +1,32 @@
+﻿using Aoc2020.Lib.Day09;
+using Aoc2020.Lib.Util;
+using Aoc2020.Tests.Day01;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests.Day09
+{
+    public class Day09Tests
+    {
+        [Theory]
+        [InlineData("Day09/Example1.txt", 5, 127)]
+        [InlineData("Day09/Input.txt", 25, 104054607)]
+        public void Part1(string filepath, int preamble, int expected)
+        {
+            var parser = new Parser(filepath);
+            var lines = parser.Parse<long>(new XMAXFactory());
+            var sut = new XMASEncrypter(lines.ToArray(), preamble);
+            var actual = sut.Cypher();
+            Assert.Equal(expected, actual.Value);
+        }
+    }
+
+    internal class XMAXFactory : IParseFactory<long>
+    {
+        public long Create(Line line)
+        {
+            return Convert.ToInt64(line.Raw);
+        }
+    }
+}
