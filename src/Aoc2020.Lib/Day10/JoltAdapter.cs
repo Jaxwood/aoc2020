f25f46c1e37326343a5ac62d08f25dd777f393c7
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-10 06:34:37 +0100

    Solution for part 1

---
+++ src/Aoc2020.Lib/Day10/JoltAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day10
+{
+    public class JoltAdapter
+    {
+        private IEnumerable<int> adapters;
+
+        public JoltAdapter(IEnumerable<int> adapters)
+        {
+            var tmp = adapters.ToList();
+            tmp.Add(tmp.Max() + 3);
+            tmp.Sort();
+            this.adapters = tmp;
+        }
+
+        public int Chain()
+        {
+            var oneDiff = 0;
+            var threeDiff = 0;
+            var current = 0;
+            foreach (var adapter in this.adapters)
+            {
+                var diff = adapter - current;
+                if (diff == 1)
+                {
+                    oneDiff++;
+                }
+                if (diff == 3)
+                {
+                    threeDiff++;
+                }
+                
+                current = adapter;
+
+            }
+            return oneDiff * threeDiff;
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day10/Day10Tests.cs
@@ -0,0 +1,23 @@
+﻿using Aoc2020.Lib.Day10;
+using Aoc2020.Lib.Util;
+using Aoc2020.Tests.Day01;
+using Xunit;
+
+namespace Aoc2020.Tests.Day10
+{
+    public class Day10Tests
+    {
+        [Theory]
+        [InlineData("Day10/Example1.txt", 35)]
+        [InlineData("Day10/Example2.txt", 220)]
+        [InlineData("Day10/Input.txt", 2201)]
+        public void Part1(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory());
+            var sut = new JoltAdapter(lines);
+            var actual = sut.Chain();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
