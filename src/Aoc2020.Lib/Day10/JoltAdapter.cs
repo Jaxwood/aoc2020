c5ffb344f706963c323758b743c5aa88c8efb583
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-10 20:48:19 +0100

    Feature/day10 (#10)
    Solution for day10

---
+++ src/Aoc2020.Lib/Day10/JoltAdapter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Aoc2020.Lib.Day10
+{
+    public class JoltAdapter
+    {
+        private IEnumerable<int> adapters;
+        private HashSet<int> adapterSet;
+
+        public JoltAdapter(IEnumerable<int> adapters)
+        {
+            var tmp = adapters.Concat(new int[] { 0, adapters.Max() + 3 }).ToList();
+            tmp.Sort();
+            this.adapters = tmp;
+            this.adapterSet = new HashSet<int>(tmp);
+        }
+
+        public int Chain()
+        {
+            var oneDiff = 0;
+            var threeDiff = 0;
+            var current = 0;
+            foreach (var adapter in this.adapters)
+            {
+                var diff = adapter - current;
+                if (diff == 1)
+                {
+                    oneDiff++;
+                }
+                if (diff == 3)
+                {
+                    threeDiff++;
+                }
+
+                current = adapter;
+
+            }
+            return oneDiff * threeDiff;
+        }
+
+        public long Pathways()
+        {
+            var branches = new Dictionary<int, long>();
+            foreach (var adapter in this.adapters)
+            {
+                var pathways = this.Connections(adapter);
+                branches.Add(adapter, pathways.Count());
+            }
+
+            foreach (var adapter in this.adapters.Reverse())
+            {
+                if (branches[adapter] > 1)
+                {
+                    branches[adapter] = this.Connections(adapter)
+                        .Aggregate(0L, (acc, n) => acc + NextBranchCount(n, branches));
+                }
+            }
+
+            return branches.First(kv => kv.Value > 1).Value;
+        }
+
+        private long NextBranchCount(int con, IDictionary<int, long> branches)
+        {
+            return this.adapters
+                .SkipWhile(c => c != con)
+                .Where(c => branches[c] > 1)
+                .Select(c => branches[c])
+                .DefaultIfEmpty(1L)
+                .First();
+        }
+
+        private IEnumerable<int> Connections(int current)
+        {
+            if (this.adapterSet.Contains(current + 1))
+            {
+                yield return current + 1;
+            }
+            if (this.adapterSet.Contains(current + 2))
+            {
+                yield return current + 2;
+            }
+            if (this.adapterSet.Contains(current + 3))
+            {
+                yield return current + 3;
+            }
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day10/Day10Tests.cs
@@ -0,0 +1,36 @@
+﻿using Aoc2020.Lib.Day10;
+using Aoc2020.Lib.Util;
+using Aoc2020.Tests.Day01;
+using Xunit;
+
+namespace Aoc2020.Tests.Day10
+{
+    public class Day10Tests
+    {
+        [Theory]
+        [InlineData("Day10/Example1.txt", 35)]
+        [InlineData("Day10/Example2.txt", 220)]
+        [InlineData("Day10/Input.txt", 2201)]
+        public void Part1(string filepath, int expected)
+        {
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory());
+            var sut = new JoltAdapter(lines);
+            var actual = sut.Chain();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Day10/Example1.txt", 8)]
+        [InlineData("Day10/Example2.txt", 19208)]
+        [InlineData("Day10/Input.txt", 169255295254528)]
+        public void Part2(string filepath, long expected)
+        {
+            var parser = new Parser(filepath);
+            var lines = parser.Parse(new IdentityFactory());
+            var sut = new JoltAdapter(lines);
+            var actual = sut.Pathways();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
