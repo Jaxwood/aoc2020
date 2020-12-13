ddf3c02d0e73aced52e4ad34eef32251fbac6a98
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-13 08:00:36 +0100

    Feature/day12 (#12)
    Solution for day12

---
+++ src/Aoc2020.Lib/Day12/Coordinate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Aoc2020.Lib.Day12
+{
+    public record Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public void Update(Move move)
+        {
+            switch (move.Direction)
+            {
+                case Direction.East:
+                    this.X += move.Units;
+                    break;
+                case Direction.West:
+                    this.X -= move.Units;
+                    break;
+                case Direction.North:
+                    this.Y += move.Units;
+                    break;
+                case Direction.South:
+                    this.Y -= move.Units;
+                    break;
+                default:
+                    throw new Exception($"unsupported direction {move.Direction}");
+            }
+        }
+
+        public void Rotate(Move move)
+        {
+            for (int i = 0; i < move.Units; i += 90)
+            {
+                var oldX = this.X;
+                var oldY = this.Y;
+                this.X = move.Direction == Direction.Left ? -1 * oldY : oldY;
+                this.Y = move.Direction == Direction.Left ? oldX : -1 * oldX;
+            }
+        }
+
+        public Coordinate Multiply(Move move)
+        {
+            return new Coordinate(this.X * move.Units, this.Y * move.Units);
+        }
+
+        public int Distance()
+        {
+            return Math.Abs(this.X) + Math.Abs(this.Y);
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = this.X;
+            y = this.Y;
+        }
+
+        public static Coordinate operator +(Coordinate a, Coordinate b)
+            => new Coordinate(a.X + b.X, a.Y + b.Y);
+    }
+}
---
+++ src/Aoc2020.Lib/Day12/WaypointShip.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aoc2020.Lib.Day12
+{
+    public class WaypointShip
+    {
+        private IEnumerable<Move> moves;
+        private Coordinate coord;
+        private Coordinate waypoint;
+
+        public WaypointShip(IEnumerable<Move> moves)
+        {
+            this.moves = moves;
+            this.coord = new (0, 0);
+            this.waypoint = new (10, 1);
+        }
+
+        public int Sail()
+        {
+            foreach (var move in this.moves)
+            {
+                switch (move.Direction)
+                {
+                    case Direction.East:
+                    case Direction.West:
+                    case Direction.North:
+                    case Direction.South:
+                        this.waypoint.Update(move);
+                        break;
+                    case Direction.Left:
+                    case Direction.Right:
+                        this.waypoint.Rotate(move);
+                        break;
+                    case Direction.Forward:
+                        this.coord += this.waypoint.Multiply(move);
+                        break;
+                    default:
+                        throw new Exception($"unknown direction {move.Direction}");
+                }
+            }
+
+            return this.coord.Distance();
+        }
+    }
+}
