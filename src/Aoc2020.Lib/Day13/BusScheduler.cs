ba47c98fe2587e1eac6a076fb17a1bf7e3d1a54f
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-13 06:41:39 +0100

    Solution for part 1

---
+++ src/Aoc2020.Lib/Day13/BusScheduler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day13
+{
+    public class BusScheduler
+    {
+        private readonly IEnumerable<(int, int)> schedules;
+
+        public BusScheduler(string schedules)
+        {
+            var segments = schedules.Split(',');
+            var result = new List<(int, int)>();
+            for (int i = 0; i < segments.Length; i++)
+            {
+                if (segments[i] != "x")
+                {
+                    result.Add((i, Convert.ToInt32(segments[i])));
+                }
+            }
+            this.schedules = result;
+        }
+
+        public int Schedule(int departureTime)
+        {
+            var bestTimeSlot = Int32.MaxValue;
+            var bestBus = 0;
+            foreach (var (_, schedule) in this.schedules)
+            {
+                var timeslot = this.Generate(schedule).SkipWhile(s => s < departureTime).First();
+                if (timeslot < bestTimeSlot)
+                {
+                    bestBus = schedule;
+                    bestTimeSlot = timeslot;
+                }
+            }
+
+            return (bestTimeSlot - departureTime) * bestBus;
+        }
+
+        public IEnumerable<int> Generate(int interval)
+        {
+            var num = interval;
+            while (true)
+            {
+                yield return num;
+                num += interval;
+            }
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day13/Day13Tests.cs
@@ -0,0 +1,17 @@
+﻿using Aoc2020.Lib.Day13;
+using Xunit;
+
+namespace Aoc2020.Tests.Day13
+{
+    public class Day13Tests
+    {
+        [Theory]
+        [InlineData("7,13,x,x,59,x,31,19", 939, 295)]
+        [InlineData("23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,479,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,373,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19", 1000417, 171)]
+        public void Part1(string schedule, int departureTime, int expected)
+        {
+            var sut = new BusScheduler(schedule);
+            Assert.Equal(expected, sut.Schedule(departureTime));
+        }
+    }
+}
