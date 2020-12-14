86136c01d66553c3b5300b79e5ffbcd954369a84
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-14 19:22:26 +0100

    Feature/day13 (#14)
    Solution for day13

---
+++ src/Aoc2020.Lib/Day13/BusScheduler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Aoc2020.Lib.Day13
+{
+    public class BusScheduler
+    {
+        private readonly IEnumerable<(int, int)> schedules;
+
+        public BusScheduler(string schedules)
+        {
+            var segments = schedules.Split(',');
+            var result = new List<(int, int)>();
+            for (int i = 0; i < segments.Length; i++)
+            {
+                if (segments[i] != "x")
+                {
+                    result.Add((i, Convert.ToInt32(segments[i])));
+                }
+            }
+            this.schedules = result;
+        }
+
+        public int Schedule(int departureTime)
+        {
+            var bestTimeSlot = Int32.MaxValue;
+            var bestBus = 0;
+            foreach (var (_, schedule) in this.schedules)
+            {
+                var timeslot = this.Generate(schedule).SkipWhile(s => s < departureTime).First();
+                if (timeslot < bestTimeSlot)
+                {
+                    bestBus = schedule;
+                    bestTimeSlot = timeslot;
+                }
+            }
+
+            return (bestTimeSlot - departureTime) * bestBus;
+        }
+
+        public BigInteger ScheduleByOffset()
+        {
+            var nums = this.schedules.Select(s => new BigInteger(s.Item2));
+            var remainders = this.schedules.Select(s => new BigInteger(s.Item2 - s.Item1));
+
+            return this.ChineseRemainder(nums, remainders);
+        }
+
+        private BigInteger ChineseRemainder(IEnumerable<BigInteger> numbers, IEnumerable<BigInteger> remainder)
+        {
+            var product = numbers.Aggregate(BigInteger.One, (acc, n) => acc * n);
+
+            return numbers
+                .Zip(remainder, (num, rem) => (num, rem))
+                .Aggregate(BigInteger.Zero, (acc, next) => 
+                    acc + next.rem * BigInteger.ModPow(BigInteger.Divide(product, next.num), next.num - 2, next.num) * BigInteger.Divide(product, next.num)
+                ) % product;
+        }
+
+        public IEnumerable<int> Generate(int interval)
+        {
+            var num = interval;
+            while (true)
+            {
+                yield return num;
+                num += interval;
+            }
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day13/Day13Tests.cs
@@ -0,0 +1,30 @@
+﻿using Aoc2020.Lib.Day13;
+using Xunit;
+
+namespace Aoc2020.Tests.Day13
+{
+    public class Day13Tests
+    {
+        [Theory]
+        [InlineData("7,13,x,x,59,x,31,19", 939, 295)]
+        [InlineData("23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,479,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,373,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19", 1000417, 171)]
+        public void Part1(string schedule, int departureTime, int expected)
+        {
+            var sut = new BusScheduler(schedule);
+            Assert.Equal(expected, sut.Schedule(departureTime));
+        }
+
+        [Theory]
+        [InlineData("17,x,13,19", 3417)]
+        [InlineData("67,7,59,61", 754018)]
+        [InlineData("67,x,7,59,61", 779210)]
+        [InlineData("67,7,x,59,61", 1261476)]
+        [InlineData("1789,37,47,1889", 1202161486)]
+        [InlineData("23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,479,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,373,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19", 539746751134958)]
+        public void Part2(string schedule, long expected)
+        {
+            var sut = new BusScheduler(schedule);
+            Assert.Equal(expected, sut.ScheduleByOffset());
+        }
+    }
+}
