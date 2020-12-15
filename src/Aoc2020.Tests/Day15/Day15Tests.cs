f29914eeb484c1e2a0f99d2fa46c2841056fc20c
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-15 07:48:06 +0100

    Solution for part 1

---
+++ src/Aoc2020.Tests/Day15/Day15Tests.cs
@@ -0,0 +1,24 @@
+﻿using Aoc2020.Lib.Day15;
+using Xunit;
+
+namespace Aoc2020.Tests.Day15
+{
+    public class Day15Tests
+    {
+        [Theory]
+        [InlineData("0,3,6", 436)]
+        [InlineData("1,3,2", 1)]
+        [InlineData("2,1,3", 10)]
+        [InlineData("1,2,3", 27)]
+        [InlineData("2,3,1", 78)]
+        [InlineData("3,2,1", 438)]
+        [InlineData("3,1,2", 1836)]
+        [InlineData("13,16,0,12,15,1", 319)]
+        public void Part1(string numbers, long expected)
+        {
+            var sut = new MemoryGame(numbers, 2020);
+            var actual = sut.Play();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
