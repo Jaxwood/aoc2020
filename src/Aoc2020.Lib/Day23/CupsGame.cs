e014e5636b42dff990a5f5f17b016f6cc0ed7217
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-23 08:22:21 +0100

    Solution for part 1

---
+++ src/Aoc2020.Lib/Day23/CupsGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day23
+{
+    public class CupsGame
+    {
+        private int[] cups;
+
+        public CupsGame(int[] cups)
+        {
+            this.cups = cups;
+        }
+
+        public string Play(int rounds)
+        {
+            int round = 1;
+            int index = 0;
+            while (round <= rounds)
+            {
+                var current = this.cups[index];
+                var pickedUpCups = this.Generate(index).Take(3).ToArray();
+                var cupsLeft = this.Generate(index).Skip(3).Take(this.cups.Length - 3).ToArray();
+                var destination = this.FindDestinationCup(current, cupsLeft);
+                this.cups = cupsLeft.Take(destination).Concat(pickedUpCups).Concat(cupsLeft.Skip(destination)).ToArray();
+                round++;
+
+            }
+
+            var idxOne = Array.FindIndex(this.cups, c => c == 1);
+            return this.Generate(idxOne).Take(8).Aggregate("", (acc, n) => acc + n);
+        }
+
+        private int FindDestinationCup(int current, int[] cupsLeft)
+        {
+            while (true)
+            {
+                current--;
+                if (current <= 0)
+                {
+                    return this.GuardAgainstOverflow(Array.FindIndex(cupsLeft, c => c == cupsLeft.Max()) + 1);
+                }
+                var cupIdx = Array.FindIndex(cupsLeft, c => c == current);
+                if (cupIdx != -1)
+                {
+                    return this.GuardAgainstOverflow(cupIdx + 1);
+                }
+            }
+        }
+
+        private IEnumerable<int> Generate(int from)
+        {
+            while (true)
+            {
+                from = this.GuardAgainstOverflow(from + 1);
+                yield return this.cups[from];
+            }
+        }
+
+        private int GuardAgainstOverflow(int idx)
+        {
+            return idx == this.cups.Length ? 0 : idx;
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day23/Day23Tests.cs
@@ -0,0 +1,21 @@
+﻿using Aoc2020.Lib.Day23;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests.Day23
+{
+    public class Day23Tests
+    {
+        [Theory]
+        [InlineData("389125467", 10, "92658374")]
+        [InlineData("389125467", 100, "67384529")]
+        [InlineData("538914762", 100, "54327968")]
+        public void Part1(string cups, int rounds, string expected)
+        {
+            var sut = new CupsGame(cups.Select(c => Convert.ToInt32(c.ToString())).ToArray());
+            var actual = sut.Play(rounds);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
