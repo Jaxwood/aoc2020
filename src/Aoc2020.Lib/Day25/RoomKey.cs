567f32fa6f6e9d2ce673c21c1af3ac9b377eaac2
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-25 09:38:21 +0100

    Solution for day25

---
+++ src/Aoc2020.Lib/Day25/RoomKey.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aoc2020.Lib.Day25
+{
+    public class RoomKey
+    {
+        public KeyMetadata Handshake(long subjectNumber, Predicate<KeyMetadata> condition)
+        {
+            var loopSize = 1L;
+            var value = 1L;
+            while (true)
+            {
+                value *= subjectNumber;
+                value %= 20201227;
+                var key = new KeyMetadata(value, loopSize);
+                if (condition(key))
+                {
+                    return key;
+                }
+                loopSize++;
+            }
+        }
+
+        public long EncryptionKey(long cardkey, long doorkey)
+        {
+            var secret = this.Handshake(7L, (key) => key.Key == cardkey);
+            var encryptionKey = this.Handshake(doorkey, (key) => key.Secret == secret.Secret);
+            return encryptionKey.Key;
+        }
+    }
+
+    public record KeyMetadata(long Key, long Secret);
+}
---
+++ src/Aoc2020.Tests/Day25/Day25Tests.cs
@@ -0,0 +1,20 @@
+﻿using Aoc2020.Lib.Day25;
+using Xunit;
+
+namespace Aoc2020.Tests.Day25
+{
+    public class Day25Tests
+    {
+        [Theory]
+        [InlineData(5764801, 17807724, 14897079)]
+        [InlineData(17807724, 5764801, 14897079)]
+        [InlineData(10441485, 1004920, 17032383)]
+        [InlineData(1004920, 10441485, 17032383)]
+        public void Part1(long cardkey, long doorkey, long expected)
+        {
+            var sut = new RoomKey();
+            var actual = sut.EncryptionKey(cardkey, doorkey);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
