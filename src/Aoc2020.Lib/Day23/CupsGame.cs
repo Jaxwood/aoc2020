6cce762aa3c8ae5d270620c730e6b9804a3bf254
Jacob Lorenzen <jacob@lorenzen.me>
2020-12-26 16:01:34 +0100

    Feature/day23 (#25)
    Solution for day23

---
+++ src/Aoc2020.Lib/Day23/CupsGame.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Aoc2020.Lib.Day23
+{
+    public class CupsGame
+    {
+        private int[] cups;
+        private int max;
+
+        public CupsGame(int[] cups)
+        {
+            this.cups = cups;
+            this.max = cups.Max();
+        }
+
+        public int[] Play(int rounds)
+        {
+            int round = 0;
+            int current = this.cups[0];
+            this.Setup();
+
+            while (round < rounds)
+            {
+                var cupsToMove = this.GetNextThreeCups(current);
+                var next = this.GetDestinationCup(current, cupsToMove);
+                this.cups[current] = this.cups[cupsToMove[2]];
+                this.cups[cupsToMove[2]] = this.cups[next];
+                this.cups[next] = cupsToMove[0];
+                current = this.cups[current];
+                round++;
+            }
+
+            return this.cups;
+        }
+
+        private int GetDestinationCup(int current, int[] threeCups)
+        {
+            var candidate = current - 1 == 0 ? this.max : current - 1;
+            while (threeCups.Contains(candidate))
+            {
+                candidate--;
+                if (candidate == 0) candidate = this.max;
+            }
+            return candidate;
+
+        }
+
+        private int[] GetNextThreeCups(int current)
+        {
+            var one = this.cups[current];
+            var two = this.cups[one];
+            var three = this.cups[two];
+            return new int[] { one, two, three };
+        }
+
+        private void Setup()
+        {
+            var result = new int[this.cups.Length + 1];
+            for (int i = 0; i < this.cups.Length; i++)
+            {
+                result[this.cups[i]] = i + 1 == this.cups.Length ? this.cups[0] : this.cups[i + 1];
+            }
+            this.cups = result;
+        }
+    }
+}
---
+++ src/Aoc2020.Tests/Day23/Day23Tests.cs
@@ -0,0 +1,52 @@
+﻿using Aoc2020.Lib.Day23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Xunit;
+
+namespace Aoc2020.Tests.Day23
+{
+    public class Day23Tests
+    {
+        [Theory]
+        [InlineData("389125467", 10, "92658374")]
+        [InlineData("389125467", 100, "67384529")]
+        [InlineData("538914762", 100, "54327968")]
+        public void Part1(string cups, int rounds, string expected)
+        {
+            var sut = new CupsGame(cups.Select(c => Convert.ToInt32(c.ToString())).ToArray());
+            var actual = sut.Play(rounds);
+            var result = "";
+            var index = 1;
+
+            while (true)
+            {
+                if (actual[index] == 1) break;
+                result += actual[index];
+                index = actual[index];
+            }
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("389125467", 10_000_000, 149_245_887_792)]
+        [InlineData("538914762", 10_000_000, 157_410_423_276)]
+        public void Part2(string cups, int rounds, long expected)
+        {
+            var cupsArr = cups.Select(c => Convert.ToInt32(c.ToString())).ToArray();
+            var sut = new CupsGame(cupsArr.Concat(this.Generate(cupsArr.Max())).ToArray());
+            var actual = sut.Play(rounds);
+            Assert.Equal(expected, BigInteger.Multiply(actual[1], actual[actual[1]]));
+        }
+
+        private IEnumerable<int> Generate(int from)
+        {
+            while (from < 1_000_000)
+            {
+                from = from + 1;
+                yield return from;
+            }
+        }
+    }
+}
